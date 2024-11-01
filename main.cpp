90c934549ee8dda671afa37afd31a9c7977d7d6d
qgm <poloqgm@gmail.com>
2024-11-01 21:46:10 +0100

    تم انهاء دوال مقارنة و حساب

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 // كلاس لتحكم بنافدة
 class Display
 {
-   public:
+public:
     void ws(int i)
     {
         if (i == 0)
@@ -22,6 +22,7 @@ class Display
                  << "\t\t\t\t  |    1  to Operations    |\n"
                  << "\t\t\t\t  |    2 to Comparisons    |\n"
                  << "\t\t\t\t  |       3 to about       |\n"
+                 << "\t\t\t\t  |         4 exit         |\n"
                  << "\t\t\t\t  |                        |\n"
                  << "\t\t\t\t  |________________________|\n"
                  << "\n\n"
@@ -93,35 +94,121 @@ class Display
 // كلاس الاساسي للمهام خاصة برنامج
 class Tasks
 {
+
 private:
+    char op;
+    double x;
+    double y;
+    double z;
+
 public:
+    void cin_(int a)
+    {
+        if (a == 1)
+        {
+            cout << "enter Operations :" << endl;
+            cin >> x >> op;
+            if (op == 's' || op == 'S' || op == 'c' || op == 't')
+                ;
+            else
+            {
+                cin >> y;
+            }
+        }
+        else if (a == 2)
+        {
+            cout << "Enter first number : ";
+            cin >> x;
+            cout << "Enter second number : ";
+            cin >> y;
+        }
+    }
+    void Operations()
+    {
+        switch (op)
+        {
+        case '+':
+            cout << x + y << endl;
+            break;
+        case '-':
+            cout << x - y << endl;
+            break;
+        case '*':
+            cout << x * y << endl;
+            break;
+        case '/':
+            cout << x / y << " The remainder of the division is " << fmod(x, y) << endl;
+            break;
+        case 'p':
+            cout << pow(x, y) << endl;
+            break;
+        case 's':
+            cout << sqrt(x) << endl;
+            break;
+        case 'S':
+            cout << sin(x) << endl;
+            break;
+        case 'c':
+            cout << cos(x) << endl;
+            break;
+        case 't':
+            cout << tan(x) << endl;
+            break;
+
+        default:
+            cerr << "error op != 's' || op !='S'||op != 'c'|| op!='t'|| op != '+'|| op != '-'|| op != '*'|| op != '/' " << endl;
+            break;
+        }
+    }
+    void Comparisons()
+    {
+        if (x > y || y > x)
+        {
+            cout << "The big number is " << max(x, y) << " , The small number is " << min(x, y) << endl;
+        }
+        else
+        {
+            cout << "Two equal numbers " << x << " = " << y << endl;
+        }
+    }
+    void about()
+    {
+    }
 };
 
 int main()
 {
     int a;
     Display dio;
+    Tasks to;
     dio.ws(0);
-    dio.scz(800,600);
+    dio.scz(800, 600);
     dio.dmb();
     dio.dr();
-    cin>>a;
+    cin >> a;
     switch (a)
     {
     case 1:
         dio.ws(a);
-        sleep(3);
+        to.cin_(a);
+        to.Operations();
         break;
     case 2:
         dio.ws(a);
-        sleep(3);
+        to.cin_(a);
+        to.Comparisons();
         break;
     case 3:
         dio.ws(a);
-        sleep(3);
+        to.about();
         break;
-    
+    case 4:
+        
+        break;
     default:
+        cerr<<"error a < "<<a<<"\a"<<endl;
+        sleep(0.8);
+        system("cls");
         break;
     }
     getchar();
