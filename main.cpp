51c9ef8a210dda1aa45c89bccb4b9114a37064fd
qgm <poloqgm@gmail.com>
2024-11-01 20:53:20 +0100

    تم تهيئة مشورع للعمل

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <unistd.h>
+#include <windows.h>
+#include <cmath>
+#include <fcntl.h>
+
+using namespace std;
+
+// كلاس لتحكم بنافدة
+class Display
+{
+    public:
+
+};
+
+// كلاس الاساسي للمهام خاصة برنامج
+class Tasks 
+{
+    private:
+
+    public:
+        
+};
+
+int main(){
+
+    return 0;
+}
